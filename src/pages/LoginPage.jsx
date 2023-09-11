4371369b601ade7101ec042bcc752986908632ae
Begimai Mukaeva <begimaimukaeva2001@gmail.com>
2023-09-12 00:53:53 +0600

    after change

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -84,7 +84,7 @@ const handleSignIn = async (e) => {
               <input
                 className='sign-in-input'
                 type="text"
-                placeholder="Введи туда-сюда логин"
+                placeholder="Введи туда-сюда адрес электронной почты"
                 value={login}
                 onChange={e => setLogin(e.target.value)}
                 required
